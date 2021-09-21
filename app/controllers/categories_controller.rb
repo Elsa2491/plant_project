da684ff9b1140db29eeb21e1aea6d2d7e85fbb2b
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-09-21 16:26:53 +0200

    try to solve category pb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,47 +1,44 @@
 class CategoriesController < ApplicationController
-  #before_action :set_category, only: %i[show edit update destroy]
+  before_action :set_category, only: %i[show edit update destroy]
 
   def index
-   # @categories = Category.all
+    @categories = Category.all
   end
 
   def show
   end
 
   def new
-    #@category = Category.new
+    @quiz = Quiz.find(params[:quiz_id])
+    @category = Category.new
   end
 
   def create
-    # @category = Category.create(category_params)
-    # @category.save
-    # redirect_to categories_path
+    @category = Category.create(category_params)
+    @category.save
+    redirect_to quiz_categories_path
   end
 
   def edit
   end
 
   def update
-    # @category.update(category_params)
-    # redirect_to categories_path
+    @category.update(category_params)
+    redirect_to category_path
   end
 
   def destroy
-    # @category.destroy
-    # redirect_to categories_path
-  end
-
-  def destroy_all
-   # Category.where(catgory: @category).destroy_all
+    @category.destroy
+    redirect_to category_path
   end
 
   private
 
   def category_params
-    # params.require(:category).permit(:name)
+    params.require(:category).permit(:name)
   end
 
   def set_category
-   # @category = Category.find(params[:id])
+   @category = Category.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :plants #, only: [:index, :show]
-  resources :quizzes, :categories do
-    resources :questions
+  resources :quizzes do
+    resources :categories, shallow: true do
+      resources :questions, shallow: true
+    end
   end
 
+  resources :categories, only: [:edit, :update, :destroy]
+  resources :questions, only: [:edit, :update, :destroy]
   root to: 'plants#index'
 end
