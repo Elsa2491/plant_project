5970301f1210d73a1116a3a759d2933f1966e1d6
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-10-26 18:48:49 +0200

    test ok

--- test/system/plants_test.rb
+++ test/system/plants_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class PlantsTest < ApplicationSystemTestCase
   test "visiting the index" do
-    visit root_url
+    visit '/plants'
 
     assert_selector "h1", text: "Les plantes"
     assert_selector '.card-product', count: Plant.count
