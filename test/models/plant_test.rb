54964dfef6ddb84e1b5d9ab555b62db7bde49c62
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-12-03 19:14:16 +0100

    remove tests

--- test/models/plant_test.rb
+++ test/models/plant_test.rb
@@ -4,8 +4,4 @@ class PlantTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "all caracteristics are capitalized" do
-    plant = Plant.new(name: 'nouvelle plante')
-    assert_equal "Nouvelle plante", plant.capitalization
-  end
 end
